0a339f0df5d5babd811f152714ce6cd870125647
Niilo <niilo.illikainen22@gmail.com>
2025-09-16 09:30:19 +0300

    viikko1 valmis

--- src/main/java/viikko1/ArrayStats.java
+++ src/main/java/viikko1/ArrayStats.java
@@ -1,12 +1,27 @@
 package viikko1;
 
 public class ArrayStats {
-    public static double average(int[] arr) {
-        // TODO
-        return 0.0;
+     public static double average(int[] arr) {
+        if (arr.length == 0){
+            return 0.0;
+        }
+        long sum = 0;
+        for (int x : arr){
+            sum += x;
+        }
+        return (double) sum / arr.length;
     }
-    public static int max(int[] arr) {
-        // TODO
-        return Integer.MIN_VALUE;
-    }
+    
+     public static int max(int[] arr) {
+        if (arr.length == 0){
+            return Integer.MIN_VALUE;
+        }
+        int largest = arr[0];
+        for (int x : arr){
+            if (x > largest){
+                largest = x;
+            }
+        }
+        return largest;
+    } 
 }
